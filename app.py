76a9d93694553c56910a818ff5641bffaca13ca5
Kpoze <redmezouaghi@gmail.com>
2024-03-22 11:20:16 +0100

    update

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-import requests 
+import requests
+
+url = "https://invoiceocrp3.azurewebsites.net/invoices"
+
+headers = {
+    "accept": "application/json"
+}
+
+
+response = requests.get(url, headers=headers)
+data = response.json()
+data = data.get('invoices')
+lastDate = data[-1]['dt']
+flag = False
+count = 0
+while flag == False : 
+    urldata  = f"https://invoiceocrp3.azurewebsites.net/invoices?start_date={lastDate}"
+    responseData = requests.get(urldata, headers=headers)
+    if responseData.status_code == 200:
+            adddata = responseData.json()
+            if len(adddata) == 0:
+                adddata = adddata.get('invoices')
+                data.extend(adddata)
+                lastDate = data[-1]['dt']
+                print(lastDate,data[-1])
+            else:
+                flag = True
+    else :
+        print(f"Erreur: {response.status_code} - {response.reason}") 
+        flag = True
+ 
+if flag == True:
+    getDatatest = data[5]['no']
+    getData  = f"https://invoiceocrp3.azurewebsites.net/invoices?start_date={getDatatest}"
+    print(getDatatest)  # Affiche les données JSON de la réponse
